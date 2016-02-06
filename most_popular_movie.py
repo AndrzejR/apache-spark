6391a1dfc0e045086223055c1b53d91c0e9394fb
Andrzej Ruszczewski <andrzej.ruszczewski@gmail.com>
2016-02-06 15:21:40 +0100

    Adding names to the movies with a broadcast variable.

--- most_popular_movie.py
+++ most_popular_movie.py
@@ -10,6 +10,16 @@ from pyspark import SparkConf, SparkContext
 conf = SparkConf().setMaster('local').setAppName('most_popular_movie')
 sc = SparkContext(conf=conf)
 
+def load_movie_names():
+	movie_names = {}
+	with open('c:/SparkCourse/ml-100k/u.item') as f:
+		for line in f:
+			line = line.split('|')
+			movie_names[int(line[0])] = line[1] # index needs to be an int, just as it is cast below, else: index error
+	return movie_names
+
+name_dict = sc.broadcast(load_movie_names())
+
 lines = sc.textFile('c:/SparkCourse/ml-100k/u.data')
 parse_lines = lines.map(lambda x: int(x.split()[1]))
 # MovieID
@@ -21,7 +31,11 @@ movie_rating_counts = prep_movies.reduceByKey(lambda x,y: x+y)
 # MovieID_grouped, Count
 
 sorted_mrc = movie_rating_counts.map(lambda x: (x[1], x[0])).sortByKey(ascending=False)
+# Count, MovieID_grouped order by 1
 
-the_most_popular_movie = sorted_mrc.first()
+sorted_mrc_names = sorted_mrc.map(lambda x: (x[0], name_dict.value[x[1]])) # 'Broadcast' object does not support indexing => .value
+# Count, MovieName_grouped order by 1
 
-print("MovieID: " + str(the_most_popular_movie[1]) + " is the most popular, with " + str(the_most_popular_movie[0]) + " ratings.")
+the_most_popular_movie = sorted_mrc_names.first()
+
+print(str(the_most_popular_movie[1]) + " is the most popular, with " + str(the_most_popular_movie[0]) + " ratings.")
